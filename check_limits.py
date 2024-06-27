c012e587fc3dcbbd6989954ab8911d05426d2a42
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-06-27 18:17:58 +0530

    Update check_limits.py-5

--- check_limits.py
+++ check_limits.py
@@ -4,7 +4,6 @@ def compare(value, lowerBound, upperBound):
 def battery_is_ok(temperature, soc, charge_rate):
     battery_ok = 0
     temp_ok = compare(temperature, 0, 45)
-    temp_ok = (temperature < 0 or temperature > 45)
     soc_ok = compare(soc, 20, 80)
     charge_ok = (charge_rate > 0.8)
     battery_ok = temp_ok + soc_ok + charge_ok

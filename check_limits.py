2a2b711c235e96043db79273417b09b8d792b7c1
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-06-27 18:14:00 +0530

    Update check_limits.py-4

--- check_limits.py
+++ check_limits.py
@@ -1,8 +1,11 @@
-
+def compare(value, lowerBound, upperBound):
+    return (value < lowerBound or value > upperBound);
+    
 def battery_is_ok(temperature, soc, charge_rate):
     battery_ok = 0
+    temp_ok = compare(temperature, 0, 45)
     temp_ok = (temperature < 0 or temperature > 45)
-    soc_ok = (soc < 20 or soc > 80)
+    soc_ok = compare(soc, 20, 80)
     charge_ok = (charge_rate > 0.8)
     battery_ok = temp_ok + soc_ok + charge_ok
     print(battery_ok)
@@ -10,9 +13,9 @@ def battery_is_ok(temperature, soc, charge_rate):
     if battery_ok >= 1:
         print('Battery is Not okay!')
         return False
-        
-    print('Battery is okay!')
-    return True
+    else:
+        print("Battery is okay!")
+        return True
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)

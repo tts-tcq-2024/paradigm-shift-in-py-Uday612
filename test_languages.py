b603f94d7e0f4322864a597188d7458bdf4c42fb
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-06 01:11:57 +0530

    Create test_languages.py

---
+++ test_languages.py
@@ -0,0 +1,14 @@
+import unittest
+from languages import get_message
+
+class TestLanguagesSupported(unittest.TestCase):
+
+  def test_english(self):
+    self.assertEqual(get_message(english,"temperature"),"Temperature is out of range")
+    self.assertEqual(get_message(english,"soc"),"Temperature is out of range")
+    self.assertEqual(get_message(english,"charge"),"Temperature is out of range")
+
+  def test_german(self):
+    self.assertEqual(get_message(german,"temperature"),"Die Temperatur liegt außerhalb des zulässigen Bereichs")
+    self.assertEqual(get_message(german,"soc"),"Der SOC liegt außerhalb des zulässigen Bereichs")
+    self.assertEqual(get_message(german,"charge"),"Die Ladung liegt außerhalb der Reichweite")

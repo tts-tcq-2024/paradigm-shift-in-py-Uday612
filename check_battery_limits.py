2066aea05ca870560d0b7b6eca126b8a2a738cde
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-05 23:51:49 +0530

    Update and rename check_limits.py to check_battery_limits.py

--- check_battery_limits.py
+++ check_battery_limits.py
@@ -2,15 +2,11 @@ def check_in_range(value, lowerBound, upperBound):
     return (value > lowerBound and value < upperBound);
     
 def battery_is_ok(temperature, soc, charge_rate):
-    battery_status = [check_in_range(temperature, 0, 45), 
+    parameter_status = [check_in_range(temperature, 0, 45), 
     check_in_range(soc, 20, 80), 
     (charge_rate <= 0.8)]
     
-    for value in battery_status:
-        if not value:
+    for status in parameter_status:
+        if not status:
             return False
     return True
-
-if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)

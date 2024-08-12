9854cf9a0572de224c62ab32ad87b140f9d2065b
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-12 14:33:41 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,3 +1,6 @@
+from languages import get_message
+parameters = ["temperature","soc","charge_rate"]
+
 def check_in_range(value, lower_bound, upper_bound):
     return (value > lower_bound and value < upper_bound);
 
@@ -12,7 +15,14 @@ def charge_in_range(charge_rate):
 
 def battery_is_ok(temperature, soc, charge_rate):
     parameter_status = [temperature_in_range(temperature), soc_in_range(soc), charge_in_range(charge_rate)]
+    parameter_index = 0
     for status in parameter_status:
         if not status:
+            global parameters
+            print_message(get_message(parameters[parameter_index]))
             return False
+        parameter_index +=1
     return True
+
+def print_message(message):
+    print(message)

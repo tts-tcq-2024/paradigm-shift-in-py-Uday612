7f486acbf61189df2d5f757d72e294999cee625b
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-12 13:54:04 +0530

    Update test_languages.py

--- test_languages.py
+++ test_languages.py
@@ -5,19 +5,21 @@ class TestLanguagesSupported(unittest.TestCase):
 
   def test_set_preferred_language(self):
     set_preferred_language("english")
-    self.assertEqual(set_preferred_language,"english")
+    self.assertEqual(preferred_language,"english")
     set_preferred_language("german")
-    self.assertEqual(set_preferred_language,"english")
+    self.assertEqual(preferred_language,"german")
     
   def test_english(self):
-    self.assertEqual(get_message("english","temperature"),"Temperature is out of range")
-    self.assertEqual(get_message("english","soc"),"Temperature is out of range")
-    self.assertEqual(get_message("english","charge"),"Temperature is out of range")
+    set_preferred_language("english")
+    self.assertEqual(get_message("temperature"),"Temperature is out of range")
+    self.assertEqual(get_message("soc"),"Temperature is out of range")
+    self.assertEqual(get_message("charge"),"Temperature is out of range")
 
   def test_german(self):
-    self.assertEqual(get_message("german","temperature"),"Die Temperatur liegt außerhalb des zulässigen Bereichs")
-    self.assertEqual(get_message("german","soc"),"Der SOC liegt außerhalb des zulässigen Bereichs")
-    self.assertEqual(get_message("german","charge"),"Die Ladung liegt außerhalb der Reichweite")
+    set_preferred_language("german")
+    self.assertEqual(get_message("temperature"),"Die Temperatur liegt außerhalb des zulässigen Bereichs")
+    self.assertEqual(get_message("soc"),"Der SOC liegt außerhalb des zulässigen Bereichs")
+    self.assertEqual(get_message("charge"),"Die Ladung liegt außerhalb der Reichweite")
 
 if __name__ == '__main__':
     unittest.main()
